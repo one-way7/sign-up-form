52e920bd154611f4c7a2bde2ffa78c0f0bc486ff
Vladislav <vladislav.work98@gmail.com>
2023-10-26 17:19:41 +0300

    a check has been created that verifies that the password fields match each other

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.querySelector('form');
+    const password = document.querySelector('#password');
+    const confirmedPassword = document.querySelector('#confirmed_password');
+
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        if (!password.length >= 8) return;
+       
+        if (password.value === confirmedPassword.value) {
+            password.nextElementSibling.classList.remove('invalid-password');
+            
+            prompt('The form has been successfully submitted')
+        } else {
+            password.nextElementSibling.classList.add('invalid-password');
+            return;
+        }
+    })
+})
